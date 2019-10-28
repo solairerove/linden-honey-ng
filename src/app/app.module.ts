b8956cc95b0dd065455c4246283240fcf79027d7
Mikita Kryvitski <solairerove@gmail.com>
2019-10-28 23:54:54 +0300

    feature: add song details stub component - song service stub as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongDetailsComponent } from './song-details/song-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongDetailsComponent
   ],
   imports: [
     BrowserModule,
